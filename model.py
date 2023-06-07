44c84baed55cc102416599a2acba7779e77f57db
Chris J Abraham <ajsirch@yahoo.com>
2023-06-07 09:54:46 -0400

    Switching the matrix multiplication of the alibi matrix

--- model.py
+++ model.py
@@ -76,7 +76,7 @@ class CausalSelfAttention(nn.Module):
             y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
         else:
             # manual implementation of attention
-            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)) + self.m_factor * self.get_alibi_matrix())
+            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)) + self.get_alibi_matrix() * self.m_factor)
             att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
             att = F.softmax(att, dim=-1)
             att = self.attn_dropout(att)
